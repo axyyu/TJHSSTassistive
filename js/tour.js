2afeba870689b96a7120b06b5541ccd01606b8ff
Andrew Wang <andrewxiaoyu0@gmail.com>
2017-03-22 15:17:00 -0400

    Started the tour - I think we've decided to just have it on the front page if multipage redirects don't work properly

--- js/tour.js
+++ js/tour.js
@@ -50,18 +50,17 @@
             steps: [
                 {
                     element: "#getstarted",
+                    placement: "right",
+                    title: "Welcome to Techademy!",
+                    content: "This is a tutorial that will walk you through the website's features step by step."
+                }, {
+                    element: "#tutorials",
+                    placement: "top",
+                    title: "Tutorial Categories",
+                    content: "Here are the tutorial categories. Hover (move your mouse over) these squares to view each topic!"
+                }, {
+                    element: "#tutorials",
                     placement: "bottom",
-                    title: "Welcome to Bootstrap Tour!",
-                    content: "Introduce new users to your product by walking them through it step by step."
-                }, {
-                    element: "#tutorials",
-                    placement: "top",
-                    title: "A super simple setup",
-                    content: "Easy is better, right? The tour is up and running with just a few options and steps."
-                }, {
-                    path: "phone.html",
-                    element: "#tutorials",
-                    placement: "top",
                     title: "Best of all, it's free!",
                     content: "Yeah! Free as in beer... or speech. Use and abuse, but don't forget to contribute!"
                 }
